ffd258403b18f46a169167b2ca4c15fc81303ff1
Pandya Limawan <86063345+pan-dya@users.noreply.github.com>
2022-05-17 15:57:08 +0700

    Create account.java

---
+++ Forum3/account.java
@@ -0,0 +1,23 @@
+public class account {
+    public double balance;
+
+    public Account(double balance){
+        this.balance = balance;
+    }
+
+    public boolean deposit(double amount){
+        if (amount > 0){
+            this.balance += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean withdraw(double amount){
+        if (amount > 0 && this.balance > amount){
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
